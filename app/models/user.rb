990473a95ea52bf2f7b84c30395beecacf2d6cc6
masayan-06 <mdmkgift@gmail.com>
2020-03-05 00:40:48 +0900

    Users_DB

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :evaluations
+  has_many :goods
+  has_many :items
+  #has_many :items, through: :goods
+  has_many :comments
+
+  validates :name, presence: true
+
+end
