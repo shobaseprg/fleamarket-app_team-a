53087659c8810c97ecacd42be0081dadf4ba4e64
shobaseprg <pgymlsho19128@gmail.com>
2020-03-07 11:39:51 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::Base
+    before_action :basic_auth
+  
+    private
+  
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == '6465teama' && password == 'tonsuke'
+      end
+    end
+  end
 end
