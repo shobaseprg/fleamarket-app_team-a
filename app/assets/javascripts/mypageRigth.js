05604b4a7f113420194ba378b81fa6ce7d7d9952
shobaseprg <pgymlsho19128@gmail.com>
2020-03-12 20:01:01 +0900

    一覧クリックで、ボタン削除

--- app/assets/javascripts/mypageRigth.js
+++ app/assets/javascripts/mypageRigth.js
@@ -6,9 +6,17 @@ $(function()  {
 
       const index = tabs.index(this);
 
-      $(".mypageMainContainer__rightContnet__lists").removeClass("activeLists").eq(index).addClass("activeLists");
+      $(".mypageMainContainer__rightContnet__lists").removeClass("active").eq(index).addClass("active");
     console.log("ok")
     }
 
   tabs.click(tabSwitch);  
+
+  function pushAllShow(){
+    $(".mypageMainContainer__rightContnet__lists__list__allShow").addClass("non-active");
+    console.log("ok")
+    } 
+
+  $(".mypageMainContainer__rightContnet__lists__list__allShow__btn").click(pushAllShow);
+
 });
