db7fbaf3501ae9ee28384e7d1a27c880a241946f
shobaseprg <pgymlsho19128@gmail.com>
2020-03-13 17:23:00 +0900

    イニシャルページのアイテムボックス作成

--- app/assets/javascripts/mypageRigth.js
+++ app/assets/javascripts/mypageRigth.js
@@ -12,7 +12,7 @@ $(function()  {
    let allshowbtns = $(".jq-all-show-btn")
   function pushAllShowbtn(){
     $('html, body').animate({scrollTop:0},'fast');
-    $(".five-lists").addClass("non-active");
+    $(".mypageMainContainer__rightContnet__initial").addClass("non-active");
     console.log("ok");
     } 
     allshowbtns.click(pushAllShowbtn);
@@ -25,13 +25,13 @@ $(function()  {
 //   $(".t-btn").click(pushAllShowTbtn);
 //   // ▲やることから一覧を押した場合　うえやま
 
-//   let itemTabs = $(".itemTab")
-//     function itemTabSwitch() {
-//       $(".itemTab").removeClass("activeTab");
-//       $(this).addClass("activeTab");
-//     const index = itemTabs.index(this);
-//       $(".itemBox").removeClass("active").eq(index).addClass("active"); 
-//     }
-//     itemTabs.click(itemTabSwitch);
+  let initialitemtabs = $(".jq-initial-item-tab")
+    function itemTabSwitch() {
+      initialitemtabs.removeClass("activeTab");
+      $(this).addClass("activeTab");
+    const index = initialitemtabs.index(this);
+      $(".box-content").removeClass("active").eq(index).addClass("active");
+    }
+    initialitemtabs.click(itemTabSwitch);
 
 });
