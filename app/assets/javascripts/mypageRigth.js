4129aa9d16f24a7309cc3ed53b7a5a1a0c751ee4
shobaseprg <pgymlsho19128@gmail.com>
2020-03-13 13:05:49 +0900

    Revert "とりけし"
    This reverts commit cb7e2ec7bb38a45028e35c9ae4cd45e0f891a8b5.

--- app/assets/javascripts/mypageRigth.js
+++ app/assets/javascripts/mypageRigth.js
@@ -1,5 +1,4 @@
 $(function()  {
-
   let tabs = $(".tab")
     function tabSwitch() {
       $(".tab").removeClass("activeTab");
@@ -8,12 +7,9 @@ $(function()  {
       $(".mypageMainContainer__rightContnet__lists").removeClass("active").eq(index).addClass("active");
     }
   tabs.click(tabSwitch);  
-  // ▲tabを押した場合　うえやま
 
   function pushAllShowNbtn(){
     $("#five-lists").removeClass("active").addClass("non-active");
-    $("#all-tabs").removeClass("non-active").addClass("active");
-    $("#all-notice-lists").removeClass("non-active").addClass("active");
     } 
   $(".n-btn").click(pushAllShowNbtn);
   // ▲通知から一覧を押した場合　うえやま
@@ -32,6 +28,5 @@ $(function()  {
       $(".itemBox").removeClass("active").eq(index).addClass("active"); 
     }
     itemTabs.click(itemTabSwitch);
-    // ▲取引中の商品のタブを押した場合　うえやま
 
 });
