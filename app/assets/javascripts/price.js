1a8affd817dc6eda30f87fbab8722ae16b3537f1
Eri Taniguchi <koko.twi.kaza@gmail.com>
2020-03-14 14:41:43 +0900

    自動計算追加

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,19 +1,19 @@
-// $(function()  {
-//     $("#item_price").on('input',function(){
-//       let price = $(this).val();
-//       let minPrice = 300
-//       let maxPrice = 9999999
-//       let fee = 10
-//       let tax = Math.floor(price / fee)
-//       let taxProfit = tax.toLocaleString()
-//       let profit = price - tax
-//       let kanmaProfit = profit.toLocaleString()
-//       if(price >= minPrice && price <= maxPrice){
-//         $("#tax-text").text("¥" + taxProfit);
-//         $("#profit-text").text("¥" + kanmaProfit);
-//       } else {
-//         $("#tax-text").text("-");
-//         $("#profit-text").text("-");
-//       }
-//   })
-// })
+$(function(){
+    $("#item_price").on('input',function(){
+      let price = $(this).val();
+      let minPrice = 300
+      let maxPrice = 9999999
+      let fee = 10
+      let tax = Math.floor(price / fee)
+      let taxProfit = tax.toLocaleString()
+      let profit = price - tax
+      let kanmaProfit = profit.toLocaleString()
+      if(price >= minPrice && price <= maxPrice){
+        $("#tax-text").text("¥" + taxProfit);
+        $("#profit-text").text("¥" + kanmaProfit);
+      } else {
+        $("#tax-text").text("-");
+        $("#profit-text").text("-");
+      }
+    })
+  })
