32054a5fcfad20d2aa0e61c9853dbc614e3365c9
shobaseprg <pgymlsho19128@gmail.com>
2020-03-22 22:49:11 +0900

    Revert "修正"
    This reverts commit 8e9527de5178d721df3b33a6aeb920d7d51599de.

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,92 +1,52 @@
-# class CardController < ApplicationController
+class CardController < ApplicationController
 
-#   require "payjp"
-
-#   def new
-#     card = Card.where(user_id: current_user.id)
-#     # redirect_to action: "show" if card.exists?
-#   end
-
-#   def create #payjpとCardのデータベース作成を実施します。
-#     Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
-#     後ほど環境変数
-#     if params['payjp-token'].blank?
-#       redirect_to action: "new"
-#     else
-#       customer = Payjp::Customer.create(
-#       description: '登録テスト', #なくてもOK
-#       # email: 'test@test', #なくてもOK
-#       card: params['payjp-token']
-#       # metadata: {user_id: current_user.id}
-#       ) #念の為metadataにuser_idを入れましたがなくてもOK
-#       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-#       if @card.save
-#         redirect_to action: "show"
-#       else
-#         redirect_to action: "pay"
-#       end
-#     end
-#   end
-
-#   def delete #P'ayjpとCardデータベースを削除します
-#     card = Card.where(user_id: current_user.id).first
-#     if card.blank?
-#     else
-#       Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
-#       customer = Payjp::Customer.retrieve(card.customer_id)
-#       customer.delete
-#       card.delete
-#     end
-#       redirect_to action: "new"
-#   end
-
-#   def show #Cardのデータpayjpに送り情報を取り出します
-#     card = Card.where(user_id: current_user.id).first
-#     if card.blank?
-#       redirect_to action: "new" 
-#     else
-#       Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
-#       # customer = Payjp::Customer.retrieve(card.customer_id)
-#       # @default_card_information = customer.cards.retrieve(card.card_id)
-#     end
-#   end
-# end
-
-class CardsController < ApplicationController
   require "payjp"
-  before_action :set_card
 
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
-    card = Card.where(user_id: current_user.id).first
+  def new
+    card = Card.where(user_id: current_user.id)
+    # redirect_to action: "show" if card.exists?
   end
 
- # indexアクションはここでは省略
-
-  def create #PayjpとCardのデータベースを作成
+  def pay #payjpとCardのデータベース作成を実施します。
     Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
-
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
-      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
-        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
-        email: current_user.email,
-        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
-        metadata: {user_id: current_user.id} # 無くてもOK。
-      )
+      description: '登録テスト', #なくてもOK
+      # email: 'test@test', #なくてもOK
+      card: params['payjp-token']
+      # metadata: {user_id: current_user.id}
+      ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "index"
+        redirect_to action: "show"
       else
-        redirect_to action: "create"
+        redirect_to action: "pay"
       end
     end
   end
 
-  private
+  def delete #P'ayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
+  end
 
-  def set_card
-    @cards = Card.where(user_id: current_user.id) if Card.where(user_id: current_user.id).present?
+  def show #Cardのデータpayjpに送り情報を取り出します
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = 'sk_test_0c40fd2bbc8f1aac67d8caba'
+      # customer = Payjp::Customer.retrieve(card.customer_id)
+      # @default_card_information = customer.cards.retrieve(card.card_id)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :items
 
-  resources :card, only: [:new, :show, :create] do
+  resources :card, only: [:new, :show] do
     collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
     end
   end
