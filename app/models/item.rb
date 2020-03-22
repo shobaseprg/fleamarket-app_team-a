1cd8a2835b0aa09b4ffa92a2614ea01f2d07bc85
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-03-22 20:46:06 +0900

    仮のビューまではOK

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,14 @@ class Item < ApplicationRecord
   validates_associated :item_images
   validates :images, presence: true
 
+  belongs_to :category
+  belongs_to :brand
   has_many :goods
   has_many :comments
   belongs_to :buyer, class_name: "User" 
   belongs_to :saller, class_name: "User"
-  belongs_to :brand
-  belongs_to :category
   has_many :users_transacts, dependent: :destroy 
   has_one :to_do, dependent: :destroy 
-  has_one :shipping, dependent: :destroy
 
   with_options presence: true do
     validates :name
