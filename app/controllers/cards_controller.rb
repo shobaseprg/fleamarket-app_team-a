f1947893d9a75ed4dea9d824e07922bf0282c6d1
shobaseprg <pgymlsho19128@gmail.com>
2020-03-24 23:25:47 +0900

    ストロングパロメーターキャンセル

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,17 +9,22 @@ class CardsController < ApplicationController
       redirect_to action: "new"
       # トークンが取得出来てなければループ
     else
-      user_id = current_user.id
-      customer = Payjp::Customer.create(
+      @user_id = current_user.id
+      @customer = Payjp::Customer.create(
       card: params['payjp-token']
       # params['payjp-token']（response.id）からcustomerを作成
       ) 
-      @card = Card.new(user_id: user_id, customer_id: customer.id, card_id: customer.default_card)
+      @card = Card.new(card_params)
       if @card.save
         redirect_to "/"
       else
         redirect_to action: "new"
       end
     end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(user_id: @user_id, customer_id: @customer.id, card_id: @customer.default_card)
   end
 end
