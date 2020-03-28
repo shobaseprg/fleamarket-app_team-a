63f66da1d810158bbd3c22c6569f86ef0fe8c71d
shobaseprg <pgymlsho19128@gmail.com>
2020-03-29 00:18:47 +0900

    homes修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_20_083820) do
 
   create_table "brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
