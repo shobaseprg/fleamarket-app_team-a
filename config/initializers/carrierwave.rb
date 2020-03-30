f97ab67886177ad6eed0eb345bea209bc0e28b8b
shobaseprg <pgymlsho19128@gmail.com>
2020-03-30 14:28:19 +0900

    インデント

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,21 +3,21 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
-    aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
-    region: 'ap-northeast-1'
-  }
+    if Rails.env.production?
+      config.storage = :fog
+      config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'fleamarket-app-team-a-bkt'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket-app-team-a-bkt'
-else
-  config.storage :file # 開発環境:public/uploades下に保存
-  config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-end  
+      config.fog_directory  = 'fleamarket-app-team-a-bkt'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fleamarket-app-team-a-bkt'
+    else
+      config.storage :file # 開発環境:public/uploades下に保存
+      config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+    end  
 
 end
