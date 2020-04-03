c9c34712d7bbc8c4c51b6b8d63d31b7b70f28eb6
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-04-03 18:42:53 +0900

    修正完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   # has_many :goods
   has_many :saling_items, -> { where("seller_id is not NULL && buyer_id is NULL") }, class_name: "Item"
   has_many :auction_items, -> { where("seller_id is not NULL && auction_id is not NULL && buyer_id is NULL") }, class_name: "Item"
-  has_many :sold_items, -> { where("seller_id is not NULL && buyer_id is not NULL && auction_id is NULL") }, class_name: "item"
+  has_many :sold_items, -> { where("seller_id is not NULL && buyer_id is not NULL && auction_id is NULL") }, class_name: "Item"
  
 
   # has_many :comments, dependent: :destroy
