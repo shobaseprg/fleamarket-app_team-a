e8e8679f1a83b02a068f283e8d85c04f4c055d7b
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-04-07 18:27:41 +0900

    複数枚投稿、最低限の機能完成

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,25 +1,98 @@
-$(document).on(function() {
+//$(document).onすることで常に最新のHTMLの情報を取得することができる。
+//turbolinks = Ajaxによるページ遷移の高速化のためのライブラリ
+$(function(){
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
     const html = `<div class="js-file_group" data-index="${index}">
-                    <input class="js-file" type="file"
-                    name="item[item_images_attributes][${index}][image]" 
+                    <input class="js-file_group__js-file" type="file"
+                    name="item[item_images_attributes][${index}][image]"
                     id="item_item_images_attributes_${index}_image">
                     <span class="js-remove">削除</span>
                   </div>`;
     return html;
   }
 
+  // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-  $('#image-box').on('change', '.js-file', function(e){
-    $('#imagebox').append(buildFileField(fileIndex[0]));
+  
+  // changeメソッドは、フォーム要素の内容が変更された時にイベント処理を実行できる
+  // changeメソッドは元々のHTML要素に対して有効
+  // on('change', '子要素')はjavascriptで変更された後のHTMLにも有効
+  $('#image-box').on('change', '.js-file_group__js-file', function(e) {
+    console.log("OK")
+    // fileIndexの先頭の数字を使ってinputを作る
+    $('#image-box').append(buildFileField(fileIndex[0]));
+    //shiftメソッドは0番目のインデックスの要素を削除してインデックスの連番の値をシフトし、 削除した値を返します。
     fileIndex.shift();
+    // 末尾の数に1足した数を追加する
+    //push()メソッドは配列の最後に1つ以上の要素を追加し、新しい配列のlengthを返します。
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
   });
 
   $('#image-box').on('click', '.js-remove', function() {
     $(this).parent().remove();
-    if ($('.js-file').length == 0 ) $('#image-box').append(buildFileField(fileIndex[0]));
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file_groupjs-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 });
+
+// $(function(){
+//   let images = [];
+//   const previewHTML = img => {
+//     const html =
+//       `<div class="preview">
+//         <div class="preview__img">
+//           <img src="${img}">
+//         </div>
+//         <div class="preview__delete">
+//           <div class="preview__delete--button">
+//             <p>削除</p>
+//           </div>
+//         </div>
+//       </div>`;
+//     $(".dropbox").prepend(html);
+//   };
+
+//   $(".dropbox__img").on("drop", e => {
+//     // キャンセル、ストップのメソッド
+//     e.preventDefault();
+//     e.stopPropagation();
+//     // 変数定義
+//     let dropImg = e.originalEvent.dataTransfer.files;
+//     let previewCount = $(".preview").length;
+//     let fileImput = $("#sell-img")[0];
+//     // 10枚まで
+//     if (dropImg.length == 1) {
+//       if (previewCount <= 9) {
+//         let img = URL.createObjectURL(dropImg[0]);
+//         previewHTML(img);
+//         images.push(dropImg[0].name);
+//         fileImput.files = dropImg;
+//         if (previewCount == 9) {
+//           $(".dropbox__img").remove();
+//         }
+//       }
+//     } else {
+//       alert("画像がアップロードできません");
+//     }
+//   });
+
+//   // 削除ボタンを押した時のアクション
+//   $(document).on("click", ".preview__delete--button", function() {
+//     $(this).closest(".preview").remove();
+//   });
+
+//   // それぞれのアクション
+//   $(document).on("drop", e => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//   });
+//   $(document).on("dragenter", e => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//   });
+//   $(document).on("dragover", e => {
+//     e.stopPropagation();
+//     e.preventDefault();
+//   });
+// });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,40 @@
 class ItemsController < ApplicationController
 
   def index
-
+    #@items = Item.includes(:item_images).order('created_at DESC')
   end
 
   def new
     @item = Item.new
     @item.item_images.new
+    # 1.times { @item.item_images.build }
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save!
-      flash[:notice] = "出品が完了しました"
       redirect_to root_path
     else
-      flash[:alert] = '出品に失敗しました。必須項目を確認してください。'
       redirect_to new_item_path
     end
   end
 
+     # 親カテゴリーが選択された後に動くアクション
+  def category_children
+      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+      @category_children = Category.find(params[:parent_id]).children
+  end
+  
+   # 子カテゴリーが選択された後に動くアクション
+   def category_grandchildren
+      @category_grandchildren = Category.find(params[:child_id]).children
+   end
+
   private
   def item_params
-    params.require(:item).permit(:name, :description_item, :condition_id, :shipping_charger_id, :shipping_method_id, :ship_from_id, :shipping_days_id, :price, :sales_profit, item_images_attributes: [:image])
+    params.require(:item).permit(:name, :description_item, :brand_id, :category_id, :condition_id, :shipping_charger_id, :shipping_method_id, :ship_from_id, :shipping_days_id, :price, :sales_profit, item_images_attributes: [:image])
   end
 
 end
+
 
