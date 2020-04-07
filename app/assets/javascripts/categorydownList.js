d9bd9768d7ebf50cfd620eebee97500b5b69839b
shobaseprg <pgymlsho19128@gmail.com>
2020-04-08 00:22:18 +0900

    ゆっくりならok

--- app/assets/javascripts/categorydownList.js
+++ app/assets/javascripts/categorydownList.js
@@ -27,67 +27,46 @@ $(function(){
       $("#mago").removeClass("non-show-list").addClass("show-list");
       });
 
+      function childrenDownBuild(children){
+        $("#ko").empty();
+        $.each(children,
+          function(index,child) {
+          let html = 
+          // <div class= "category-downlist__in__one" data-id="<%= child.id %>">
+          `<div class= "category-downlist__in__one" data-id = ${child.id}>
+            <a href= "#">${child.name}</a>
+          </div>`
 
-          $(".category-downlist__in__one").hover(function () {
-            console.log("ok")
-            let parentID = $(this).data('id'); //選択されたカテゴリーのカスタム属性data-id（つまり、そのかてごりーのid)を格納
-            if (parentID != "") { //親カテゴリーが初期値でないことを確認
-              $.ajax({
-                  // リクエストを送信する先のURL
-                  url: "/items/category_children",
-                  // HTTP通信の種類を記述する
-                  type: "GET",
-                  // サーバに送信する値
-                  data: {
-                    parent_id: parentID
-                  },
-                  // サーバから返されるデータの型
-                  dataType: "json"
-                })
-                // doneはAjax通信が成功したとき,failはAjax通信が失敗したとき
-                .done(function (children) {
-                  $("#child_box").remove();
-                  $("#grandchild_box").remove();
-                  let insertHTML = "";
-                  children.forEach(function (child) {
-                    insertHTML += appendOption(child);
-                  });
-                  childBox(insertHTML);
-                  $('#parent_form').css('margin', '0');
-                }).fail(function () {
-                  alert("カテゴリー取得に失敗しました");
-                })
-            } else {
-              $("#child_box").remove();
-              $("#grandchild_box").remove();
-            }
-          })
+          $("#ko").append(html);
+          });
+      }
 
-        $(function () {
-          $("#category-select-box_list").on('change', "#child_form", function () {
-            let childValue = $("#child_form").val();
-            if (childValue != "") {
-              $.ajax({
-                url: "/items/category_grandchildren",
-                type: "GET",
-                data: {
-                  child_id: childValue
-                },
-                dataType: "json"
-              }).done(function (grandchilds) {
-                $("#grandchild_box").remove()
-                let insertHTML = "";
-                grandchilds.forEach(function (grandchild) {
-                  insertHTML += appendOption(grandchild);
-                });
-                grandChildBox(insertHTML);
-                $('#child_form').css('margin', '0');
-              }).fail(function () {
-                alert("カテゴリー取得に失敗しました");
-              })
-            } else {
-              $("#grandchild_box").remove()
-            }
-          })
-        })
+
+
+      $(".category-downlist__in__one").mouseover(function () {
+        let parentID = $(this).data('id'); //選択されたカテゴリーのカスタム属性data-id（つまり、そのかてごりーのid)を格納
+          $.ajax({
+              // リクエストを送信する先のURL
+              url: "/items/category_children",
+              // HTTP通信の種類を記述する
+              type: "GET",
+              // サーバに送信する
+              // parent_idをparamsに格納しておくる。params[:parent_id]
+              data: {
+                parent_id: parentID
+              },
+              // サーバから返されるデータの型
+              dataType: "json"
+            })
+            // doneはAjax通信が成功したとき,failはAjax通信が失敗したとき
+            .done(function (children) {
+              // childrenにはjson形式で帰ってきたデータが格納されている。
+              childrenDownBuild(children);
+
+            }).fail(function () {
+              alert("カテゴリー取得に失敗しました");
+            })
+        
+        });
+    
 });
