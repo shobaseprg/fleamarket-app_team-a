7186b9c89704034e121936221bb1124789c730b3
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-04-09 09:29:42 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_current_user_items,only:[:i_OnSale,:i_trading,:i_SoldOut]
   before_action :set_user,only:[:i_OnSale,:i_trading,:i_SoldOut]
-  before_action :set_item, except: [:index, :new, :create]
+ # before_action :set_item, except: [:index, :new, :create]
 
   def i_OnSale #出品中のアクション
 
@@ -47,21 +47,21 @@ class ItemsController < ApplicationController
     @category_grandchildren = Category.find(params[:child_id]).children
   end
 
-  def edit
-  end
+  # def edit
+  # end
  
-  def update
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
  
-  def destroy
-    @product.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   @product.destroy
+  #   redirect_to root_path
+  # end
 
   private
 
@@ -76,12 +76,12 @@ class ItemsController < ApplicationController
 
 
   def item_params
-    params.require(:item).permit(:name, :description_item, :brand_id, :category_id, :condition_id, :shipping_charger_id, :shipping_method_id, :ship_from_id, :shipping_days_id, :price, :sales_profit, item_images_attributes: [:image, :_destroy, :id])
+    params.require(:item).permit(:name, :description_item, :brand_id, :category_id, :condition_id, :shipping_charger_id, :shipping_method_id, :ship_from_id, :shipping_days_id, :price, item_images_attributes: [:image])
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
 end
 
