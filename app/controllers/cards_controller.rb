13caf30050008b7a599112447357db70190c1a43
masayan-06 <mdmkgift@gmail.com>
2020-04-17 03:12:58 +0900

    購入確認_住所表示

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -46,4 +46,60 @@ class CardsController < ApplicationController
     end
   end
 
+<<<<<<< Updated upstream
 end
+=======
+  def show 
+      if @card.blank?
+        #登録された情報がない場合にカード登録画面に移動
+        redirect_to cards_path
+      else
+        #保管した顧客IDでpayjpから情報取得
+        set_customer
+        #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+        set_card_information
+      end
+  end
+
+  def pay
+    @item.update(buyer_id: current_user.id)
+    # 現在のユーザーを購入者に登録
+    Payjp::Charge.create(
+    :amount => @item.price, 
+    :customer => @card.customer_id, 
+    :currency => 'jpy', #日本円
+   )
+    redirect_to item_purchase_index_path(@item.id)
+  # 購入確認画面に遷移
+  end
+
+  private
+
+  def set_card
+    @item = Item.find(params[:id])
+    @address = Address.find(params[:id])
+  end
+
+  def card_present
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+  end
+
+  def set_api_key
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
+  end
+
+
+  def set_customer
+    @customer = Payjp::Customer.retrieve(@card.customer_id)
+  end
+
+  def set_card_information
+    @card_information = @customer.cards.retrieve(@card.card_id)
+  end
+
+  def take_card
+    @card = Card.find_by(user_id: current_user.id)
+  end
+  
+end
+>>>>>>> Stashed changes
