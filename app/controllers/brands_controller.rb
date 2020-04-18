fc89fa485bb2b408b761c1c8fe22a04d58f1db71
Eri Taniguchi <koko.twi.kaza@gmail.com>
2020-04-18 19:37:51 +0900

    ブランドhaml

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,10 @@
 class BrandsController < ApplicationController
   def index
+    @items1 = Item.where(brand_id:1).order("id DESC").last(10)
+    @items2 = Item.where(brand_id:2).order("id DESC").last(10)
+    @items3 = Item.where(brand_id:3).order("id DESC").last(10)
+    @items4 = Item.where(brand_id:4).order("id DESC").last(10)
+    @items5 = Item.where(brand_id:5).order("id DESC").last(10)
   end
 
 end
