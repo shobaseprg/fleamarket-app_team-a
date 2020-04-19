dc1f74bf336c7cd20df06bdf59f3874880faa210
masayan-06 <mdmkgift@gmail.com>
2020-04-20 04:26:29 +0900

    password_visualization(微修正)

--- app/assets/javascripts/registrations_new.js
+++ app/assets/javascripts/registrations_new.js
@@ -5,10 +5,10 @@ $(function(){
     const passwordLabel = document.querySelector('.js-password-label');
     if ($(this).prop('checked')){
       $(password).attr('type','text');
-      passwordLabel.innerHTML = '<i class="fas fa-eye-slash" style="font-size:20px;color:#808080"></i>';
+      passwordLabel.innerHTML = '<i class="fas fa-eye" style="font-size:20px;color:#808080"></i>';
     } else {
       $(password).attr('type','password');
-      passwordLabel.innerHTML = '<i class="fas fa-eye" style="font-size:20px;color:#808080"></i>';
+      passwordLabel.innerHTML = '<i class="fas fa-eye-slash" style="font-size:20px;color:#808080"></i>';
     }
   })
 })
