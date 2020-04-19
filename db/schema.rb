358cbd34f9d76d08590d4af148c6088aea2e6fd4
Eri Taniguchi <koko.twi.kaza@gmail.com>
2020-04-19 16:25:28 +0900

    追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_055424) do
+ActiveRecord::Schema.define(version: 2020_04_19_034554) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_first_name", null: false
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_055424) do
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["children_category_id"], name: "index_items_on_children_category_id"
+    t.index ["name"], name: "index_items_on_name"
     t.index ["parent_category_id"], name: "index_items_on_parent_category_id"
     t.index ["seller_id"], name: "index_items_on_seller_id"
   end
