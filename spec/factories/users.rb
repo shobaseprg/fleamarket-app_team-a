f7bdd8ed1c19c9a539a4bb96844989ee3b8734f9
shobaseprg <pgymlsho19128@gmail.com>
2020-04-19 22:50:54 +0900

    ユーザー新規登録のモデルテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    id{"1"}
+
     first_name{"山田"}
     family_name{"太郎"}
     first_name_reading{"ヤマダ"}
@@ -11,6 +11,8 @@ FactoryBot.define do
     birthday{"2018-06-06"}
     password              {"0000000a"}
     password_confirmation {"0000000a"}
+
+    self_introduction{"よろしくお願いいたします"}
   end
 
 end
