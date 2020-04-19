025b1bb5df5f4666ed019f8f80440d62669a3c3a
sho-takeuchi <moonlight2248@yahoo.co.jp>
2020-04-19 17:45:45 +0900

    修正

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -49,7 +49,6 @@ $(function(){
   $('#image-box').on('click', '.js-remove', function() {
     let limitFileField = $(".js-file_group:last").data("index");
     const targetIndex = $(this).parent().data('index')
-    console.log(targetIndex);
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().remove();
