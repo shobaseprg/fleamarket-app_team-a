c7a14a6a73d260b61f5de9f0cae31ca6cfc95e36
masayan-06 <mdmkgift@gmail.com>
2020-04-20 04:17:41 +0900

    password_visualization

---
+++ app/assets/javascripts/registrations_new.js
@@ -0,0 +1,14 @@
+$(function(){
+  var password = '#password';
+  var passcheck = '#js-passcheck';
+  $(passcheck).change(function(){
+    const passwordLabel = document.querySelector('.js-password-label');
+    if ($(this).prop('checked')){
+      $(password).attr('type','text');
+      passwordLabel.innerHTML = '<i class="fas fa-eye-slash" style="font-size:20px;color:#808080"></i>';
+    } else {
+      $(password).attr('type','password');
+      passwordLabel.innerHTML = '<i class="fas fa-eye" style="font-size:20px;color:#808080"></i>';
+    }
+  })
+})
