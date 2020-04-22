1da393dd7da0c269582280dd91903f5549daacc5
Eri Taniguchi <koko.twi.kaza@gmail.com>
2020-04-22 23:18:53 +0900

    検索ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -110,6 +110,4 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -49,9 +49,5 @@ class Item < ApplicationRecord
     validates :seller_id
    end
 
-   def self.search(search)
-    return Item.all unless search
-    Item.where('name LIKE(?)', "%#{search}%")
-  end
 end
 
