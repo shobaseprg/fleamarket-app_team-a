305069a68260731177f42b183d4e666e6e400145
shobaseprg <pgymlsho19128@gmail.com>
2020-04-23 01:29:21 +0900

    自分の発言実装可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -97,6 +97,7 @@ class ItemsController < ApplicationController
     @oya = @item.parent_category
     @comment = Comment.new
     # form_withを使用しているためからのモデルを送る
+  @commentALL = Comment.where(item_id:params[:id])
   end
 
 
