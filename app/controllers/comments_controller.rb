0df6562f7e1c8fa513a3baca91bc72854325ca65
shobaseprg <pgymlsho19128@gmail.com>
2020-04-26 22:19:42 +0900

    コントローラーリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  
+  before_action :set_comment, only: [:update,:destroy,:restore]
+  before_action :check_user, only: [:update,:destroy,:restore]
 
   def create
     @comment = Comment.new(comment_params)
@@ -8,25 +11,22 @@ class CommentsController < ApplicationController
         format.json
       end
     else
-      flash[:aleat] = "保存できていません"
+      flash[:alert] = "保存できていません"
       redirect_to item_path(params[:id])
     end
   end
 
   def update
-    @comment = Comment.find(params[:id])
     @comment.update(delete_check:1)
     redirect_to item_path(@comment.item_id)
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to item_path(@comment.item_id)
   end
 
   def restore
-    @comment = Comment.find(params[:id])
     @comment.update(delete_check:0)
     redirect_to item_path(@comment.item_id)
   end
@@ -34,6 +34,17 @@ class CommentsController < ApplicationController
 private
   def comment_params
     params.require(:comment).permit(:comment).merge(user_id: current_user.id,item_id:params[:id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def check_user
+    unless @comment.item.seller == current_user
+      flash[:alert] = "その操作はできません"
+      redirect_to root_path
+    end
   end
 end
 
