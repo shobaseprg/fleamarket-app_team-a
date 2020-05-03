c089ddf80be7039865f5c9e8cca9bb65db8b19fd
shobaseprg <pgymlsho19128@gmail.com>
2020-05-03 16:00:31 +0900

    readME修正

--- app/models/buyed_item_notice.rb
+++
@@ -1,3 +0,0 @@
-class BuyedItemNotice < ApplicationRecord
-  #belongs_to :item
-end
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,5 @@ class Card < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :Month
   belongs_to_active_hash :Year
+  belongs_to :user
 end
